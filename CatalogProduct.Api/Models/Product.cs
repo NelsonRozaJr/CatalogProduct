32e43e5f64415c933aec2998fc1785e7e3315362
Nelson Roza Junior <jr.1403@hotmail.com>
2020-09-12 21:47:33 -0300

    Data Annotations

--- CatalogProduct.Api/Models/Product.cs
+++ CatalogProduct.Api/Models/Product.cs
@@ -7,19 +7,21 @@ namespace CatalogProduct.Api.Models
     {
         public int ProductId { get; set; }
 
-        [Required]
-        [MaxLength(80)]
+        [Required(ErrorMessage = "O nome é obrigatório")]
+        [StringLength(80, MinimumLength = 5, ErrorMessage = "O nome deve ter entre 5 e 80 caracteres")]
         public string Name { get; set; }
 
-        [Required]
-        [MaxLength(300)]
+        [Required(ErrorMessage = "A descrição é obrigatória")]
+        [StringLength(300, ErrorMessage = "A descrição deve ter no máximo {1} caracteres")]
         public string Description { get; set; }
 
         [Required]
+        // O parâmetro {0} é reservado para o nome da propriedade, os outros são parâmetros da restrição
+        [Range(1, 10000, ErrorMessage = "O preço deve estar entre {1} e {2}")]
         public decimal UnitPrice { get; set; }
 
         [Required]
-        [MaxLength(500)]
+        [StringLength(500, MinimumLength = 10)]
         public string ImageUrl { get; set; }
 
         public float Stock { get; set; }
